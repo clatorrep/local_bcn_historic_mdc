0c6ebb6a1fbe6771b76f585b9ed22f00fbe57450
Cristobal Latorre Padilla <clatorre@bcnschool.cl>
2023-11-20 17:05:09 -0300

    Test escritura BD funciona

--- classes/task/cron_task.php
+++ classes/task/cron_task.php
@@ -1,6 +1,8 @@
 <?php
 namespace local_bcn_historic_mdc\task;
 
+use context_system;
+
 class cron_task extends \core\task\scheduled_task
 {
     public function get_name() {
@@ -8,6 +10,28 @@ class cron_task extends \core\task\scheduled_task
     }
 
     public function execute() {
-        mtrace($this->get_name());
+        // Test funcional
+        global $DB;
+
+        $table = 'local_bcn_historic_mdc';
+
+        $data = (object) array(
+            'userid' => 2,
+            'courseid' => 23,
+            'coursestart'=> time(),
+            'progress' => '2 de 2',
+            'progressperc' => 100.0,
+            'progressquiz' => '1 de 1',
+            'finalgrade' => 100.0,
+            'quizdate' => time(),
+            'status' => 'Aprobado',
+            'timecreated' => time()
+        );
+
+        foreach ($data as $key => $value) {
+            mtrace("$key => $value \n");
+        }
+
+        $DB->insert_record($table, $data);
     }
 }
